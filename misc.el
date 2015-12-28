c8162174907e1123781aa2f2c5dc87ec9fd0a968
Chandan Rajendra <chandan@linux.vnet.ibm.com>
2015-12-28 22:25:37 +0530

    misc.el: Require scroll-bar.
    Signed-off-by: Chandan Rajendra <chandan@linux.vnet.ibm.com>

--- misc.el
+++ misc.el
@@ -10,7 +10,10 @@
 	  'executable-make-buffer-file-executable-if-script-p)
 
 (mouse-avoidance-mode 'animate)
+
+(require 'scroll-bar)
 (scroll-bar-mode -1)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (pending-delete-mode t)
