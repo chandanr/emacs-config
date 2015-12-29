f97dc5561bdbc3816ac5c1fce09a1d58b19fb79e
Chandan Rajendra <chandan@linux.vnet.ibm.com>
2015-12-29 10:14:13 +0530

    Create and use Redhat 9's emacs theme
    Signed-off-by: Chandan Rajendra <chandan@linux.vnet.ibm.com>

---
+++ Redhat-theme.el
@@ -0,0 +1,9 @@
+(deftheme Redhat
+  "Created 2015-12-29.")
+
+(custom-theme-set-faces
+ 'Redhat
+ '(default ((t (:inherit nil :stipple nil :background "dark slate gray" :foreground "Wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "adobe" :family "Source Code Pro"))))
+ '(cursor ((t (:background "turquoise1")))))
+
+(provide-theme 'Redhat)
--- theme.el
+++ theme.el
@@ -1,5 +1,5 @@
 (when (eq window-system 'x)
-  (load-theme 'spacemacs-dark t))
+  (load-theme 'Redhat t))
 
 (set-face-attribute 'default nil :family "Monospace" :height 90)
 
