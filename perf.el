8ba3340037ee1bb6a1ebdc8624bbaf5d8706baa6
Chandan Rajendra <chandan@linux.vnet.ibm.com>
2018-03-04 03:45:32 -0600

    perf.el: Add keybindings
    Signed-off-by: Chandan Rajendra <chandan@linux.vnet.ibm.com>

--- perf.el
+++ perf.el
@@ -13,6 +13,7 @@
    (format
     "perf probe -L %s --vmlinux=%s/%s | tee"
     function-name sjihs-btrfs-next-build-dir sjihs-vmlinux-relative-path)))
+(global-set-key (kbd "C-c k p l") 'sjihs-perf-func-line-map)
 
 (defun sjihs-perf-func-var-map (function-name)
   (interactive "sFunction name: \n")
@@ -20,6 +21,7 @@
    (format
     "perf probe -V %s --vmlinux=%s/%s | tee"
     function-name sjihs-btrfs-next-build-dir sjihs-vmlinux-relative-path)))
+(global-set-key (kbd "C-c k p v") 'sjihs-perf-func-var-map)
 
 (defun sjihs-perf-probe-add ()
   (interactive)
