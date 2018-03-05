7079809bef37fe3a1bf475576a1cf16d093f7699
Chandan Rajendra <chandan@linux.vnet.ibm.com>
2018-03-05 08:19:07 -0600

    gdb.el: Generate a disassembly of a function
    Signed-off-by: Chandan Rajendra <chandan@linux.vnet.ibm.com>

--- gdb.el
+++ gdb.el
@@ -7,9 +7,24 @@
   (when (not (boundp sjihs-var))
     (error "%s: %s variable not set" load-file-name (symbol-name sjihs-var))))
 
-(defun sjihs-gdb-map-function-offset (function-offset)
-  (interactive "sFunction offset: \n")
+(defun sjihs-execute-gdb-cmd (sub-cmd)
   (compile
    (format
-    "gdb -batch %s/%s -ex 'list *(%s)'"
-    sjihs-btrfs-next-build-dir sjihs-vmlinux-relative-path function-offset)))
+    "gdb -batch %s/%s -ex '%s'"
+    sjihs-btrfs-next-build-dir sjihs-vmlinux-relative-path sub-cmd)))
+
+(defun sjihs-gdb-map-function-offset (function-offset)
+  (interactive "sFunction offset: \n")
+  (let ((sub-cmd (format "list *(%s)"
+			 sjihs-btrfs-next-build-dir
+			 sjihs-vmlinux-relative-path
+			 function-offset)))
+    (sjihs-execute-gdb-cmd sub-cmd)))
+(global-set-key (kbd "C-c k d m") 'sjihs-gdb-map-function-offset)
+
+(defun sjihs-gdb-disassemble-function ()
+  (interactive)
+  (let* ((func-name (thing-at-point 'symbol))
+	 (sub-cmd (format "disassemble /m %s" func-name)))
+    (sjihs-execute-gdb-cmd sub-cmd)))
+(global-set-key (kbd "C-c k d d") 'sjihs-gdb-disassemble-function)
