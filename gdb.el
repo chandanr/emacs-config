df0b62cecbd3fae9b834f0e7e89b8a530ad79aa3
Chandan Rajendra <chandanrlinux@gmail.com>
2020-01-21 06:35:36 +0000

    gdb.el: Rename sjihs-execute-gdb-cmd() to sjihs-gdb-execute-gdb-cmd()
    Signed-off-by: Chandan Rajendra <chandanrlinux@gmail.com>

--- gdb.el
+++ gdb.el
@@ -7,19 +7,19 @@
   (when (not (boundp sjihs-var))
     (error "%s: %s variable not set" load-file-name (symbol-name sjihs-var))))
 
-(defun sjihs-execute-gdb-cmd (sub-cmd)
+(defun sjihs-gdb-execute-gdb-cmd (sub-cmd)
   (interactive "sGDB command: \n")
   (compile
    (format
     "gdb -batch %s/%s -ex '%s'"
     sjihs-btrfs-next-build-dir sjihs-vmlinux-relative-path sub-cmd)))
-(global-set-key (kbd "C-c k d c") 'sjihs-execute-gdb-cmd)
+(global-set-key (kbd "C-c k d c") 'sjihs-gdb-execute-gdb-cmd)
 
 (defun sjihs-gdb-map-function-offset (function-offset)
   (interactive "sFunction offset: \n")
   (let ((sub-cmd (format "list *(%s)"
 			 function-offset)))
-    (sjihs-execute-gdb-cmd sub-cmd)))
+    (sjihs-gdb-execute-gdb-cmd sub-cmd)))
 (global-set-key (kbd "C-c k d m") 'sjihs-gdb-map-function-offset)
 
 (add-to-list 'compilation-error-regexp-alist-alist
@@ -30,19 +30,19 @@
   (interactive)
   (let* ((func-name (thing-at-point 'symbol))
 	 (sub-cmd (format "disassemble /m %s" func-name)))
-    (sjihs-execute-gdb-cmd sub-cmd)))
+    (sjihs-gdb-execute-gdb-cmd sub-cmd)))
 (global-set-key (kbd "C-c k d d") 'sjihs-gdb-disassemble-function)
 
 (defun sjihs-gdb-print-struct-mem-offset (struct-name sub-cmd)
   (interactive "sStructure: \nsMember: ")
   (let ((sub-cmd (format "print &(((struct %s *)0)->%s)"
 			 struct-name sub-cmd)))
-    (sjihs-execute-gdb-cmd sub-cmd)))
+    (sjihs-gdb-execute-gdb-cmd sub-cmd)))
 (global-set-key (kbd "C-c k d o") 'sjihs-gdb-print-struct-mem-offset)
 
 (defun sjihs-gdb-print-size (identifier)
   (interactive "sIdentifier: \n")
   (let ((sub-cmd (format "print sizeof(%s)"
 			 identifier)))
-    (sjihs-execute-gdb-cmd sub-cmd)))
+    (sjihs-gdb-execute-gdb-cmd sub-cmd)))
 (global-set-key (kbd "C-c k d s") 'sjihs-gdb-print-size)
