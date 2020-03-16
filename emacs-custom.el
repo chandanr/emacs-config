fa5a66b8eeb2347fd04101c9f91c7fedcd1cb870
root <root@debian-guest>
2020-03-16 08:09:45 +0530

    emacs-custom.el: Custom terminal colors
    Signed-off-by: Chandan Rajendra <chandanrlinux@gmail.com>

---
+++ emacs-custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-file (concat sjihs-emacs-config-dir-prefix "emacs-custom.el"))
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
+ '(ediff-even-diff-A ((t (:background "color-240"))))
+ '(ediff-even-diff-B ((t (:background "color-240"))))
+ '(ediff-fine-diff-A ((t (:background "brightred"))))
+ '(ediff-fine-diff-B ((t (:background "color-22"))))
+ '(ediff-odd-diff-A ((t (:background "color-242"))))
+ '(ediff-odd-diff-B ((t (:background "color-240"))))
+ '(font-lock-comment-face ((t (:foreground "color-178"))))
+ '(font-lock-constant-face ((t (:foreground "cyan"))))
+ '(font-lock-function-name-face ((t (:foreground "color-45"))))
+ '(font-lock-keyword-face ((t (:foreground "color-147"))))
+ '(font-lock-type-face ((t (:foreground "color-48"))))
+ '(font-lock-variable-name-face ((t (:foreground "color-220"))))
+ '(region ((t (:background "color-23")))))
