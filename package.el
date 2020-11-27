4d502bf2531ca0ce7f5aedd1ef146107fdc6d954
Chandan Babu R <chandanrlinux@gmail.com>
2020-11-27 15:36:36 +0530

    package.el: Disable elpa
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- package.el
+++ package.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
