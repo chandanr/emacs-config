2d991b34b6990d50476e2c71640858c9e28e4017
Chandan Babu R <chandanrlinux@gmail.com>
2022-01-16 21:51:00 +0530

    xfs.el: Add function to compute indirect block length
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- xfs.el
+++ xfs.el
@@ -73,3 +73,34 @@
     (message "Height of tree = %s" nr-levels)
     (calc-sci-notation 1)
     (calc-eval (concat total-nr-blks "/" "1.0"))))
+
+(defun xfs-compute-bmap-indirect-len (leaf-max-recs node-max-recs maxlevels
+						    delalloc-len)
+  (setq leaf-max-recs (number-to-string leaf-max-recs))
+  (setq node-max-recs (number-to-string node-max-recs))
+  (setq delalloc-len  (number-to-string delalloc-len))
+
+  (let ((level 0)
+	done
+	(indirect-len 0)
+	(maxrecs leaf-max-recs)
+	expr)
+
+    (while (and (< level maxlevels) (not done))
+      (setq expr (concat delalloc-len "+" maxrecs "- 1"))
+      (setq delalloc-len (calc-eval expr))
+
+      (setq expr (concat "idiv(" delalloc-len "," maxrecs ")"))
+      (setq delalloc-len (calc-eval expr))
+      (setq indirect-len (+ indirect-len (string-to-number delalloc-len)))
+
+      (when (= 1 (string-to-number delalloc-len))
+	(setq indirect-len (+ indirect-len (- maxlevels (+ level 1))))
+	(setq done t))
+
+      (when (= level 0)
+	(setq maxrecs node-max-recs))
+
+      (setq level (1+ level)))
+
+    indirect-len))
