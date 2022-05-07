50b9b328199b83434fb7d9a190df2ec749e9e2dd
Chandan Babu R <chandanrlinux@gmail.com>
2022-05-07 19:26:31 +0530

    mu4e.el: Select pgp keyid based on sender's email id
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- mu4e.el
+++ mu4e.el
@@ -125,6 +125,8 @@
 	:query "flag:flagged"
 	:key ?f)))
 
+;; Use sender's email id as keyid to obtain PGP signature key
+(setq mml-secure-openpgp-sign-with-sender t)
 
 (add-hook 'gnus-part-display-hook 'message-view-patch-highlight)
 ;; mu4e-headers-toggle-full-search
