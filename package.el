663e3cd6a95004f23d81846f4d0ffb29b96abf7c
Chandan Babu R <chandanrlinux@gmail.com>
2022-07-14 11:24:05 +0530

    package.el: Set melpa url correctly and remove unnecessary package archives
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- package.el
+++ package.el
@@ -1,6 +1,4 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
